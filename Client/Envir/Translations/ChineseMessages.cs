7e2983c340d6ad317e7a548f1c22c5496ae20311
codingman <codingman@126.com>
2025-12-28 19:07:36 +0800

    汉化测试

--- Client/Envir/Translations/ChineseMessages.cs
+++ Client/Envir/Translations/ChineseMessages.cs
@@ -90,7 +90,7 @@ namespace Client.Envir.Translations
         public override string FilterConfigSaved { get; set; } = "Drop filters have been saved to your configuration";
         public override string GroupMemberLimit { get; set; } = "Group member limit has been reached";
         public override string GroupSelf { get; set; } = "You can not group with yourself";
-        public override string GroupNotLeader { get; set; } = "You are not the leader of your group.";
+        public override string GroupNotLeader { get; set; } = "你不是队长。";
         public override string GroupLFGRequestSent { get; set; } = "Request to join group has been sent.";
         public override string GroupLFGRequestDelay { get; set; } = "Cannot request to join group for another {0} seconds.";
         public override string MarketNoItemSelected { get; set; } = "Error: No Item selected.";
